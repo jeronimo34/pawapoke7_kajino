ade0a0ab62cd2355b36e8854d192e7dc2598be43
jeronimo34 <unity1017@gmail.com>
2022-01-23 16:21:57 +0900

    assetsフォルダをgit管理の対象に追加

--- src/components/SpeechBubble.vue
+++ src/components/SpeechBubble.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="speech-bubble">
     <p>{{ text }}</p>
-    <img src="@/assets/SpeechBubble.png" />
   </div>
 </template>
 
@@ -25,9 +24,11 @@ export default {
 
 #speech-bubble p {
   position: absolute;
-  background: rgb(226, 234, 230);
+  background: white;
   top: -5px;
   left: 30px;
   font-size: 24px;
+  width: 320px;
+  height: 36px;
 }
 </style>
